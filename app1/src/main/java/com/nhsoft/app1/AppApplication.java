5c7467c0466034867899713f7c210f03409cea4c
luzhenfeng66 <457030889@qq.com>
2019-10-28 09:38:03 +0800

    2019102801 修改文字大小

--- app1/src/main/java/com/nhsoft/app1/AppApplication.java
+++ app1/src/main/java/com/nhsoft/app1/AppApplication.java
@@ -5,9 +5,11 @@ package com.nhsoft.app1;
 import android.content.Context;
 import android.support.multidex.MultiDex;
 
+import com.lzf.login.ui.activity.LoginActivity;
 import com.nhsoft.base.config.ModuleLifecycleConfig;
 
 import priv.lzf.mvvmhabit.base.BaseApplication;
+import priv.lzf.mvvmhabit.crash.CaocConfig;
 
 /**
  * Created by goldze on 2018/6/21 0021.
@@ -25,10 +27,26 @@ public class AppApplication extends BaseApplication {
     @Override
     public void onCreate() {
         super.onCreate();
+        initCrash();
         //初始化组件(靠前)
         ModuleLifecycleConfig.getInstance().initModuleAhead(this);
         //....
         //初始化组件(靠后)
         ModuleLifecycleConfig.getInstance().initModuleLow(this);
+    }
+
+    private void initCrash() {
+        CaocConfig.Builder.create()
+                .backgroundMode(CaocConfig.BACKGROUND_MODE_SILENT) //背景模式,开启沉浸式
+                .enabled(true) //是否启动全局异常捕获
+                .showErrorDetails(true) //是否显示错误详细信息
+                .showRestartButton(true) //是否显示重启按钮
+                .trackActivities(true) //是否跟踪Activity
+                .minTimeBetweenCrashesMs(2000) //崩溃的间隔时间(毫秒)
+                .errorDrawable(R.mipmap.ic_launcher) //错误图标
+                .restartActivity(LoginActivity.class) //重新启动后的activity
+//                .errorActivity(YourCustomErrorActivity.class) //崩溃后的错误activity
+//                .eventListener(new YourCustomEventListener()) //崩溃后的错误监听
+                .apply();
     }
 }
